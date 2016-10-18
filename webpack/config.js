2cc26399dab9f967bdcbc6ab1d2ae0caad68113e
David Epely <esion99@gmail.com>
2016-10-18 14:22:17 +0200

    add eslint

--- webpack/config.js
+++ webpack/config.js
@@ -27,7 +27,7 @@ module.exports = {
         {
           test: /\.jsx?$/,
           include: path.join(__dirname, '../src/'),
-          loaders: ['babel-loader'],
+          loaders: ['babel-loader', 'eslint-loader'],
         },
         {
           test: /\.scss$/,
