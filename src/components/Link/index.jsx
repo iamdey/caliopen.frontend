21ab1d9ea6dcd72b41bd68f45383b0da3b9a235e
David Epely <esion99@gmail.com>
2016-10-28 14:11:41 +0200

    refactor Link add raw & text styles

--- src/components/Link/index.jsx
+++ src/components/Link/index.jsx
@@ -3,7 +3,7 @@ import { Link as BaseLink } from 'react-router';
 import classnames from 'classnames';
 import './style.scss';
 
-const Link = ({ children, className, modifiers = {}, ...props }) => {
+const Link = ({ raw, className, modifiers = {}, ...props }) => {
   const linkProps = {
     ...props,
     className: classnames(
@@ -11,17 +11,18 @@ const Link = ({ children, className, modifiers = {}, ...props }) => {
       'm-link',
       {
         'm-link--button': modifiers.button,
+        'm-link--text': !modifiers.button && !raw,
       }
     ),
   };
 
-  return <BaseLink {...linkProps}>{children}</BaseLink>;
+  return <BaseLink {...linkProps} />;
 };
 
 Link.propTypes = {
+  raw: PropTypes.bool,
   className: PropTypes.string,
   modifiers: PropTypes.shape({ button: PropTypes.bool }),
-  children: PropTypes.node.isRequired,
 };
 
 export default Link;
