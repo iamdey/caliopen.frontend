b68ada1ee9513aa168c9bf73845fcef13eec6c41
David Epely <esion99@gmail.com>
2016-11-09 17:11:16 +0100

    connect appswitcher to redux

--- src/components/Page/components/Navigation/components/ApplicationSwitcher/index.jsx
+++ src/components/Page/components/Navigation/components/ApplicationSwitcher/index.jsx
@@ -1,53 +1,19 @@
-import React, { Component } from 'react';
+import { createSelector } from 'reselect';
+import { connect } from 'react-redux';
 import Presenter from './presenter';
-import { getApplications } from '../../../../../../services/application-manager/';
+import * as ApplicationManager from '../../../../../../services/application-manager/';
 
-// import { createSelector } from 'reselect';
+const applicationListSelector = () => ApplicationManager.getApplications();
+const applicationSelector = state => state.application.applicationName;
 
-// const applicationSelector = createSelector(
-//   [
-//   (state, props) => props.ApplicationManager.getInfos(state.applicationReducer.applicationName),
-//   ],
-//   (applicationInfos) => ({
-//     currentApplication: { ...applicationInfos },
-//   })
-// );
-//
-// export class ApplicationSwitcherController {
-//   $onInit() {
-//     this.$scope.$on('$destroy', this.$ngRedux.connect((state) => applicationSelector(state, {
-//       ApplicationManager: this.ApplicationManager,
-//     }))(this));
-//   }
-//
-//   isCurrentApplication(application) {
-//     return angular.equals(application, this.currentApplication);
-//   }
-// }
+const mapStateToProps = createSelector(
+  [applicationSelector, applicationListSelector],
+  (currentApplicationName, applications) => {
+    const currentApplication = applications
+      .find(application => application.name === currentApplicationName);
 
-class ApplicationSwitcher extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isDropdownOpen: false,
-    };
+    return { currentApplication, applications };
   }
+);
 
-  render() {
-    const applications = getApplications();
-
-    const appSwitchProps = {
-      applications,
-      currentApplication: applications[0],
-      isState: () => true,
-      isDropdownOpen: this.state.isDropdownOpen,
-      onDropdownToggle: (isDropdownOpen) => {
-        this.setState({ isDropdownOpen });
-      },
-    };
-
-    return <Presenter {...appSwitchProps} />;
-  }
-}
-
-export default ApplicationSwitcher;
+export default connect(mapStateToProps)(Presenter);
--- src/components/Page/components/Navigation/components/ApplicationSwitcher/presenter.jsx
+++ src/components/Page/components/Navigation/components/ApplicationSwitcher/presenter.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Link } from '../Navbar';
 import BaseLink from '../../../../../Link';
 import Icon from '../../../../../Icon';
@@ -6,52 +6,64 @@ import Dropdown, { DropdownController } from '../../../../../Dropdown';
 import VerticalMenu, { VerticalMenuItem } from '../../../../../VerticalMenu';
 import './style.scss';
 
-const Presenter = ({
-  applications,
-  currentApplication,
-  isDropdownOpen = false,
-  onDropdownToggle,
-}) => (
-  <div className="m-application-switcher">
-    <Link to={currentApplication.route}>
-      <Icon type={currentApplication.icon} /> {`header.menu.${currentApplication.name}`}
-    </Link>
-    <DropdownController
-      toggle="co-application-switcher"
-      className="dropdown-float-right m-application-switcher__toggler"
-    >
-      <span className="show-for-sr">{'application_switcher.action.choose'}</span>
-      <Icon type={isDropdownOpen ? 'caret-up' : 'caret-down'} />
-    </DropdownController>
-    <Dropdown
-      id="co-application-switcher"
-      closeOnClick
-      raw
-      position="bottom"
-      className="m-application-switcher__dropdown"
-      onToggle={onDropdownToggle}
-    >
-      <VerticalMenu>
-        {
-          applications.filter(app => app !== currentApplication)
-            .map((application, key) => (
-              <VerticalMenuItem key={key}>
-                <BaseLink to={application.route} modifiers={{ button: true, expanded: true }}>
-                  <Icon type={application.icon} /> {`header.menu.${application.name}`}
-                </BaseLink>
-              </VerticalMenuItem>
-            ))
-        }
-      </VerticalMenu>
-    </Dropdown>
-  </div>
-);
+class Presenter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDropdownOpen: false,
+    };
+  }
+
+  render() {
+    const onDropdownToggle = () => {
+      this.setState({ isDropdownOpen: !this.state.isDropdownOpen });
+    };
+
+    return (
+      <div className="m-application-switcher">
+        <Link to={this.props.currentApplication.route}>
+          <Icon type={this.props.currentApplication.icon} /> {`header.menu.${this.props.currentApplication.name}`}
+        </Link>
+        <DropdownController
+          toggle="co-application-switcher"
+          className="dropdown-float-right m-application-switcher__toggler"
+        >
+          <span className="show-for-sr">{'application_switcher.action.choose'}</span>
+          <Icon type={this.state.isDropdownOpen ? 'caret-up' : 'caret-down'} />
+        </DropdownController>
+        <Dropdown
+          id="co-application-switcher"
+          closeOnClick
+          raw
+          position="bottom"
+          className="m-application-switcher__dropdown"
+          onToggle={onDropdownToggle}
+        >
+          <VerticalMenu>
+            {
+              this.props.applications.filter(app => app !== this.props.currentApplication)
+                .map((application, key) => (
+                  <VerticalMenuItem key={key}>
+                    <BaseLink to={application.route} modifiers={{ button: true, expanded: true }}>
+                      <Icon type={application.icon} /> {`header.menu.${application.name}`}
+                    </BaseLink>
+                  </VerticalMenuItem>
+                ))
+            }
+          </VerticalMenu>
+        </Dropdown>
+      </div>
+    );
+  }
+}
 
 Presenter.propTypes = {
   applications: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-  currentApplication: PropTypes.shape({}).isRequired,
-  isDropdownOpen: PropTypes.bool,
-  onDropdownToggle: PropTypes.func,
+  currentApplication: PropTypes.shape({
+    route: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default Presenter;
