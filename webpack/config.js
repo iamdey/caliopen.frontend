7d774b7ca3b953e6a5b5dcf2bbe2e82871e9f462
David Epely <esion99@gmail.com>
2016-11-10 16:06:28 +0100

    chore: release script for server & browser
    + conditional Dashboard plugin

--- webpack/config.js
+++ webpack/config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
-const DashboardPlugin = require('webpack-dashboard/plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const extractTextPlugin = new ExtractTextPlugin('style.css');
+const isDev = process.env.NODE_ENV === 'development';
+const DASHBOARD = process.env.DASHBOARD ? JSON.parse(process.env.DASHBOARD) : true;
+
 const initialConfig = {
   entry: [],
   plugins: [],
@@ -13,62 +14,70 @@ const initialConfig = {
 };
 
 module.exports = {
-  getBase: buildTarget => Object.assign(initialConfig, {
-    plugins: [
-      new DashboardPlugin(),
+  getBase: (buildTarget) => {
+    const extractTextPlugin = new ExtractTextPlugin('style.css');
+    const plugins = [
       new webpack.DefinePlugin({
         BUILD_TARGET: JSON.stringify(buildTarget),
         CALIOPEN_ENV: JSON.stringify(process.env.NODE_ENV),
       }),
       extractTextPlugin,
-    ],
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?$/,
-          include: path.join(__dirname, '../src/'),
-          loaders: ['babel-loader', 'eslint-loader'],
-        },
-        {
-          test: /\.css$/,
-          loader: extractTextPlugin.extract('style-loader', 'css-loader'),
-        },
-        {
-          test: /\.scss$/,
-          loader: extractTextPlugin.extract('style-loader', 'css-loader!sass-loader'),
-        },
-        {
-          test: /\.(jpe?g|png|gif|svg)$/i,
-          loaders: [
-            'file?hash=sha512&digest=hex&name=[hash].[ext]',
-            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
-          ],
-        },
-        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=image/svg+xml' },
-        { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/font-woff' },
-        { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/font-woff' },
-        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/octet-stream' },
-        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
-      ],
-      preLoaders: [
-        {
-          test: /\.scss$/,
-          include: path.join(__dirname, '../src/'),
-          loader: 'sasslint',
-        },
-      ],
-    },
-    sassLoader: {
-      includePaths: [
-        path.resolve(__dirname, '../src'),
-        path.resolve(__dirname, '../node_modules'),
-      ],
-    },
-    sasslint: {
-      configFile: path.resolve(__dirname, '../.sass-lint.yml'),
-    },
-    resolve: {
-      extensions: ['', '.js', '.jsx'],
-    },
-  }),
+    ];
+    if (DASHBOARD) {
+      const DashboardPlugin = require('webpack-dashboard/plugin');
+      plugins.push(new DashboardPlugin());
+    }
+
+    return Object.assign(initialConfig, {
+      plugins,
+      module: {
+        loaders: [
+          {
+            test: /\.jsx?$/,
+            include: path.join(__dirname, '../src/'),
+            loaders: ['babel-loader', 'eslint-loader'],
+          },
+          {
+            test: /\.css$/,
+            loader: extractTextPlugin.extract('style-loader', 'css-loader'),
+          },
+          {
+            test: /\.scss$/,
+            loader: extractTextPlugin.extract('style-loader', 'css-loader!sass-loader'),
+          },
+          {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            loaders: [
+              'file?hash=sha512&digest=hex&name=[hash].[ext]',
+              'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
+            ],
+          },
+          { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=image/svg+xml' },
+          { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/font-woff' },
+          { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/font-woff' },
+          { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/octet-stream' },
+          { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+        ],
+        preLoaders: [
+          {
+            test: /\.scss$/,
+            include: path.join(__dirname, '../src/'),
+            loader: 'sasslint',
+          },
+        ],
+      },
+      sassLoader: {
+        includePaths: [
+          path.resolve(__dirname, '../src'),
+          path.resolve(__dirname, '../node_modules'),
+        ],
+      },
+      sasslint: {
+        configFile: path.resolve(__dirname, '../.sass-lint.yml'),
+      },
+      resolve: {
+        extensions: ['', '.js', '.jsx'],
+      },
+    });
+  },
 };
